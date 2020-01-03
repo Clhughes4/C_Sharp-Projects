fff0af543ec5e6e9d203d4fc20ec3c10d2509424
Clint Hughes <cahughes27@gmail.com>
2020-01-02 16:47:48 -0800

    add Insurance Project

---
+++ CarInsuranceProject/CarInsuranceQuote/ViewModels/AdminVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsuranceQuote.ViewModels
+{
+    public class AdminVm
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmaiAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool HasDUI { get; set; }
+        public int PreviousTickets { get; set; }
+        public bool FullCoverage { get; set; }
+        public double CalculatedQuote { get; set; }
+    }
+}
---
+++ CarInsuranceProject/CarInsuranceQuote/ViewModels/QuoteVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsuranceQuote.ViewModels
+{
+    public class QuoteVm
+    {
+        public string QuotedPrice { get; set; }
+        public QuoteVm(decimal quotedPrice)
+        {
+            this.QuotedPrice = String.Format("{0:C2}", quotedPrice);
+        }
+    }
+}
